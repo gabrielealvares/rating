33090f95900e36799ec119b412c0db4d490e327f
Gabriele Alvares <suelendamasio742@gmail.com>
2023-11-27 16:55:56 -0300

    No style adicionei um transition nos números, no javaScript adicionei um eventClick nos números para mudar a cor e organizei as consts

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
+const numbers = document.querySelectorAll('.number')
  const button = document.querySelector('button')
- const hiddenSection = document.querySelector('.hidden-section')
- const main = document.querySelector('main')
- const numbers = document.querySelectorAll('.number')
  const points = document.querySelector('.points')
- const body = document.querySelector('body')
+ const main = document.querySelector('main')
+ const hiddenSection = document.querySelector('.hidden-section')
+ 
  
 
  numbers.forEach(number =>{
     number.addEventListener('click',()=>{
+        number.style.background = ' hsl(25, 97%, 53%)'
+
+        number.addEventListener('mouseleave',()=>{
+         setTimeout(()=>{
+            number.style.background =''
+         },700)
+
+        })
+
         numbers.forEach(num => num.classList.remove('selected'))
         number.classList.add('selected')
     })
@@ -18,8 +27,8 @@
 
     const numberSelect = document.querySelector('.number.selected')
     if(numberSelect){
-        const numero = numberSelect.getAttribute('data-value')
-        points.textContent = `You selected ${numero} of 5`
+        const number = numberSelect.getAttribute('data-value')
+        points.textContent = `You selected ${number} of 5`
     
     }else{
         points.textContent = `selected a number first`
